5277040cc18a1bf21e086f57fbc5b9bb81da9c5b
Preetam Sharma <preetamxharma@gmail.com>
2023-09-17 16:50:08 +0530

    completed a working calculator app

--- app/src/main/java/com/example/bcalculator/CalculatorAction.kt
+++ app/src/main/java/com/example/bcalculator/CalculatorAction.kt
@@ -1,7 +1,10 @@
 package com.example.bcalculator
 
 sealed class CalculatorAction {
-    data class Number(val number: Int) : CalculatorAction()
+    data class Number(val number: Int): CalculatorAction()
     object Clear: CalculatorAction()
     object Delete: CalculatorAction()
+    data class Operation(val operation: CalculatorOperation): CalculatorAction()
+    object Calculate: CalculatorAction()
+    object Decimal: CalculatorAction()
 }
--- app/src/main/java/com/example/bcalculator/CalculatorState.kt
+++ app/src/main/java/com/example/bcalculator/CalculatorState.kt
@@ -1,5 +1,7 @@
 package com.example.bcalculator
 
 data class CalculatorState(
-
+    val number1: String = "",
+    val number2: String = "",
+    val operation: CalculatorOperation? = null
 )
